076bc4b10ed5c8340b03d3d73d69efd53b1d8f04
Magnus <alois.marcellin@gmail.com>
2025-03-26 15:14:12 +0100

    feat: Ajout de l'inscription / connexion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 
 Route::get('/todo', [\App\Http\Controllers\TodoController::class, 'index'])->name('todo.index');
@@ -9,3 +24,5 @@ Route::delete('/todo/{todo}', [\App\Http\Controllers\TodoController::class, 'del
 Route::get('/todo/{todo}', [\App\Http\Controllers\TodoController::class, 'view'])->name('todo.view');
 Route::get('/todo/{todo}/update', [\App\Http\Controllers\TodoController::class, 'updateform'])->name('todo.updateform');
 Route::post('/todo/{todo}/update', [\App\Http\Controllers\TodoController::class, 'update'])->name('todo.update');
+
+require __DIR__.'/auth.php';
